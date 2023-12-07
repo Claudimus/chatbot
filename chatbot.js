de676ba33fc10ad11441fe2c5c92ed9f12d1fb14
Claudio8 <jccantave@gmail.com>
2023-12-07 18:01:53 -0500

    made a lot of improvements

--- chatbot.js
+++ chatbot.js
@@ -1,48 +1,126 @@
 // Chatbot JS file
 
-// Reference to the chat are and user input field.
+// Reference to the chat are and user input field from html.
 
-const chatContainer = document.getElementById("chat-container");
-const userInput = document.getElementById("user-input-box");
-const sendButton = document.getElementById("send-button");
+const chatContainer = document.getElementById("chatcontainer");
+const userInput = document.getElementById("textbox");
+const sendBtn = document.getElementById("sendBtn");
+
+let user = { message: "" };
+
+const arrayOfPossibleMessages = [
+  { message: "how are you?", response: "i'm great" },
+  { message: "hi", response: "hi !" },
+  { message: "what are you doing?", response: "not much. u ?" },
+  { message: "whats new?", response: "nothin. u ?" },
+  { message: "yo!", response: "sup?" },
+  { message: "whats good ?", response: "i cant call it. " },
+  { message: "whats up ?", response: "chillin " },
+  { message: "whats up ", response: "chillin " },
+  { message: "what up ", response: "chillin " },
+  { message: "what up? ", response: "chillin " },
+  { message: "what up ?", response: "chillin " },
+  { message: "sup? ", response: "good.u ? " },
+
+  { message: "how are you?", response: "i'm great" },
+  { message: "hi", response: "hi !" },
+  { message: "what are you doing?", response: "not much. u ?" },
+  { message: "whats new?", response: "nothin. u ?" },
+  { message: "yo!", response: "sup?" },
+  { message: "whats good ?", response: "i cant call it. " },
+  { message: "whats up ?", response: "chillin " },
+  { message: "whats up ", response: "chillin " },
+  { message: "what up ", response: "chillin " },
+  { message: "what up? ", response: "chillin " },
+  { message: "what up ?", response: "chillin " },
+  { message: "sup? ", response: "good.u ? " },
+
+  { message: "how are you?", response: "i'm great" },
+  { message: "hi", response: "hi !" },
+  { message: "what are you doing?", response: "not much. u ?" },
+  { message: "whats new?", response: "nothin. u ?" },
+  { message: "yo!", response: "sup?" },
+  { message: "whats good ?", response: "i cant call it. " },
+  { message: "whats up ?", response: "chillin " },
+  { message: "whats up ", response: "chillin " },
+  { message: "what up ", response: "chillin " },
+  { message: "what up? ", response: "chillin " },
+  { message: "what up ?", response: "chillin " },
+  { message: "sup? ", response: "good.u ? " },
+];
 
 //Add a click event listener. Adds an event listener to the "Send" button. When the button is clicked, the "sendMessage" function is called. The 'sendMessage' in the parenthesis is called a callback function.
-sendButton.addEventListener("click", sendMessage);
 
-// sendMessage Function: When the user clicks "Send," this function gets what the user typed, shows it in the chat, generates a simple bot response, shows that in the chat, and then clears the input field for the next message.
+setTimeout(function () {
+  chatbotSendMessage("Hi ! Whats your name ?");
+}, 6000);
 
-// Define the function that gets called ehen the "Send" button is clicked.
-// Get what the user typed in the input field
-// Generate a simpele response based on what the user said.
-// Show the user's message in the chat
-// Show the bot's response in the chat
-// Clear the input field for the next message
-
-function sendMessage() {
-  const userMessage = userInput.value;
-  const botResponse = generateResponse(userMessage);
-  displayMessage("You", userMessage);
-  displayMessage("Bot", botResponse);
-  userInput.value = "";
-}
-
-// generateResponse Function: This function makes a simple response from the bot, including what the user said.
-
-// Function to create a simple response from the bot
-// Return a response that includes what the user said
-
-function generateResponse(userMessage) {
-  return `You said: ${userMessage}`;
-}
-
-// displayMessage Function: This function creates a new message, sets what it says, and adds it to the chat.
-// Function to display a message in the chat
-// Create a message element
-// Set the text content of the message element
-// Add the message element to the chat area
-
-function displayMessage(sender, message) {
+function chatbotSendMessage(messageText) {
   const messageElement = document.createElement("div");
-  messageElement.textContent = `${sender}: ${message}`;
+  messageElement.classList.add("w-50");
+  messageElement.classList.add("float-left");
+  messageElement.classList.add("shadow-sm");
+  messageElement.style.margin = "10px";
+  messageElement.style.padding = "5px";
+
+  messageElement.innerHTML = `<span>Chatbot:</span> <span>${messageText} </span>`;
+
+  messageElement.animate(
+    [{ easing: "ease-in", opacity: 0.1 }, { opacity: 1 }],
+    { duration: 1000 }
+  );
   chatContainer.appendChild(messageElement);
+}
+
+function sendMessage(messageText) {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("w-50");
+  messageElement.classList.add("float-right");
+  messageElement.classList.add("shadow-sm");
+  messageElement.style.margin = "10px";
+  messageElement.style.padding = "5px";
+
+  messageElement.innerHTML = `<span>You:</span> <span>${messageText} </span>`;
+
+  messageElement.animate(
+    [{ easing: "ease-in", opacity: 0.1 }, { opacity: 1 }],
+    { duration: 500 }
+  );
+  chatContainer.appendChild(messageElement);
+}
+
+sendBtn.addEventListener("click", function (e) {
+  if (textbox.value == "") {
+    alert("Please type in a mesage.");
+  } else {
+    let messageText = textbox.value;
+    user.message = messageText;
+    sendMessage(messageText);
+    textbox.value = "";
+
+    processMessage();
+  }
+});
+
+// Define a function named processMessage
+// Create a new variable named result
+// Filter arrayOfPossibleMessages based on whether each element's message includes the user's message in a case-insensitive manner
+// Access the response property of the first element in the filtered result array
+// Set a timeout function that will execute after 1000 milliseconds (1 second)
+// Call the chatbotSendMessage function with the obtained response as an argument
+
+// Putting it all together, the arrow function (val) => val.message.includes(user.message.toLowerCase()) can be read as "for each element val in the array, check if the message property of that element includes the lowercase version of user.message."
+
+//***** val represents the things in the array. It could be named item,thing,stuff, etc.
+
+function processMessage() {
+  let result = arrayOfPossibleMessages.filter((val) =>
+    val.message.includes(user.message.toLowerCase())
+  );
+
+  let response = result[0].response;
+
+  setTimeout(function () {
+    chatbotSendMessage(response);
+  }, 1000);
 }
